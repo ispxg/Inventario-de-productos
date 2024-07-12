4592867aee5e495d7099038177db8964f7681806
ispxg <fernandezalfredo1997@gmail.com>
2024-07-12 12:24:01 +0200

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,40 @@
+$("#productForm").on("submit", function (evt) {
+  evt.preventDefault();
+
+  saveProduct();
+  deleteProduct();
+});
+
+// generar un numero aleatorio
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function saveProduct() {
+  var productName = $("#productName").val();
+  var uniqueId = getRandomInt(1, 1000);
+  localStorage.setItem(uniqueId, productName);
+  $("#productName").val("");
+  return showProductInList(productName, uniqueId);
+}
+
+function showProductInList(productName, uniqueId) {
+  const productListItem = `<li id="${uniqueId}" class="product list-group-item d-flex w-50 mx-auto">
+    <div class="col-6">${productName}</div>
+    <div class="col-6 d-flex justify-content-end">
+      <button id="deleteProduct" type="button" class="btn-danger mx-auto">Delete</button>
+    </div>
+  </li>`;
+
+  return $("#listOfProducts").append(productListItem);
+}
+
+function deleteProduct() {
+  $("#listOfProducts").on("click", "#deleteProduct", function () {
+    var itemIdToRemove = $(this).parent().parent().attr("id");
+    $(this).parent().parent().remove();
+    localStorage.removeItem(itemIdToRemove);
+  });
+}
